cdb4d0e7c7d063c3942f7518ca391e37e9ca8b07
Jay Madden <jaymaddencox@gmail.com>
2021-11-22 15:08:25 -0500

    Solved WireEnds

--- src/Wire Ends/WireEndsTests.cs
+++ src/Wire Ends/WireEndsTests.cs
@@ -1,18 +1,93 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Microsoft.VisualBasic;
 
 namespace Wire_Ends
 {
+    public static class Extensions
+    {
+        public static IEnumerable<T> GetUniqueFlags<T>(this T flags) where T : Enum =>
+            Enum.GetValues(flags.GetType())
+                .Cast<Enum>()
+                .Where(flags.HasFlag)
+                .Select(value => (T) value);
+    }
+    
     [TestClass]
     public class WireEndsTests
     {
+        [Flags]
+        enum Direction : short
+        {
+            Up = 1,
+            Down= 2,
+            Left = 4,
+            Right = 8,
+            Empty = 16
+        }
+        
         public int CountWireEnds(string s)
         {
-            string chars = "║╠╩╦═╬╣╚╝╔╗ \n";
-            // -----------------------
-            // complete challenge here
-            // -----------------------
-            throw new NotImplementedException(); // <- delete
+            var chars = "║╠╩╦═╬╣╚╝╔╗ \n";
+
+            var directions = new Dictionary<string, Direction>
+            {
+                {"║", Direction.Up | Direction.Down},
+                {"╠", Direction.Up | Direction.Down | Direction.Right},
+                {"╩", Direction.Up | Direction.Left | Direction.Right},
+                {"╦", Direction.Down | Direction.Left | Direction.Right},
+                {"═", Direction.Left | Direction.Right},
+                {"╬", Direction.Left | Direction.Up | Direction.Down | Direction.Right},
+                {"╣", Direction.Left | Direction.Up | Direction.Down},
+                {"╚", Direction.Up | Direction.Right},
+                {"╝", Direction.Up | Direction.Left},
+                {"╔", Direction.Down | Direction.Right},
+                {"╗", Direction.Down | Direction.Left},
+                {" ", Direction.Empty }
+            };
+
+            var actions = new Dictionary<Direction, Func<List<List<string>>, int, int, int>>
+            {
+                {
+                    Direction.Up, 
+                    (array, i, j) => i == 0 || !directions[array[i - 1][j]].HasFlag(Direction.Down) ? 1 : 0
+                },
+                {
+                    Direction.Down, 
+                    (array, i, j) => i >= array.Count - 1 || !directions[array[i + 1][j]].HasFlag(Direction.Up) ? 1 : 0
+                },
+                {
+                    Direction.Left,
+                    (array, i, j) => j == 0 || !directions[array[i][j - 1]].HasFlag(Direction.Right) ? 1 : 0
+                },
+                {
+                    Direction.Right,
+                    (array, i, j) => j >= array[i].Count - 1 || !directions[array[i][j + 1]].HasFlag(Direction.Left) ? 1 : 0
+                },
+                { Direction.Empty, (_, _, _) => 0 }
+            };
+            var looseEndCount = 0;
+
+            var array = s.Split("\n")
+                .Select(x =>
+                    x.Select(y =>
+                        y.ToString())
+                        .ToList())
+                .ToList();
+
+            for (var i = 0; i < array.Count; i++)
+            {
+                for (var j = 0; j < array[i].Count; j++)
+                {
+                    looseEndCount += directions[array[i][j]]
+                        .GetUniqueFlags()
+                        .Sum(flag => actions[flag](array, i, j));
+                }
+            }
+            return looseEndCount;
         }
 
         [TestMethod]
